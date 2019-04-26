000f6518857f2b5f0edd2e558159a5795abd67a3
Jakub Brzezowski <44787968+zdzwie@users.noreply.github.com>
2019-04-26 15:42:24 +0200

    Dodanie kolejnych metod

--- Matrix.h
+++ Matrix.h
@@ -5,11 +5,29 @@
 #include<assert.h>
 using namespace std;
 
+template<size_t N>
+struct Matrix_slice {
+	Matrix_slice() = default;
+
+	Matrix_slice(size_t s, initializer_list<size_t> exts);
+	Matrix_slice(size_t s, initializer_list<size_t> exts, initializer_list<size_t> strs);
+
+	template<typename... Dims>
+	Matrix_slice(Dims... dims);
+
+	template<typename... Dims, typename = Enable_ifAll(Convertible<Dims, size_t>()...) >>
+		size_t operator()(Dims... dims) const;
+
+	size_t size;
+	size_t start;
+	array<size_t, N> extends;
+	array<size_t, N> strides;
+};
 template<typename T, size_t N>
 class Matrix{
 private:
 	Matrix_silce<N> desc;
-	vector<T> elems;
+	std::vector<T> elems;
 public:
 
 	static constexpr size_t order = N;
@@ -38,7 +56,7 @@ public:
 
 	//Initialization from list
 	Matrix(Matrix_initializer<T, N>);
-	Matrix& operator+(const Matrix_ref<U, N>&);
+	Matrix& operator=(const Matrix_ref<U, N>&);
 
 	template<typename U>
 	Matrix(initializer_list<U>) = delete;
@@ -58,10 +76,65 @@ public:
 	T* data() { return elems.data(); }
 	const T*data() const { return elems.data(); }
 
+	template<typename... Args>
+		Enable_if<Matrix_impl::Requesting_element<Args...>(), T&>
+		operator()(Args... args);
+	template<typename... Args>
+		Enable_if<Matrix_impl::Requesting_element<Args...>(), const T&>
+		operator()(Args... args) const;
+	template<typename... Args>
+		Enable_if<Matrix_impl::Requesting_slice<Args...>(), Matrix_ref<T, N>>
+		operator()(const Args&... args);
+	template<typename... Args>
+		Enable_if<Matrix_impl::Requesting_slice<Args...>(), Matrix_ref<const T, N>>
+		operator()(const Args&... args) const;
 
+	//m[i] Row access
+	Matrix_ref<T, N - 1> operator[](size_t i) { return row(i); }
+	Matrix_ref<const T, N - 1> operator[](size_t i) const { return row(i); }
 
+	//Row access
+	Matrix_ref<T, N - 1> row(size_t n);
+	Matrix_ref<const T, N - 1> row(size_t n) const;
+
+	//Column access
+	Matrix_ref<T, N - 1> col(size_t n);
+	Matrix_ref<const T, N - 1> col(size_t n) const;
+
+	//f(x) for every element x
+	template<typename F>
+	Matrix& apply(F f);
+
+	//f(x, mx) for corresponding elements
+	template<typename M, typename F>
+	Matrix& apply(const M& m, F f);
+
+	//Assigment with scalar
+	Matrix& operator=(const T& value);
+
+	//Scalar operations. 
+	Matrix& operator+=(const T& value);
+	Matrix operator+(const Matrix<T, N>&, const T&);
+	
+	Matrix& operator-=(const T& value);
+
+	Matrix& operator*=(const T& value);
+	Matrix<T, 2> operator*(const Matrix<T, 1>&, const Matrix<T, 1>&);
+
+	Matrix& operator/=(const T& value);
+	Matrix& operator%=(const T& value);
+
+	//Matrix operations.
+	template<typename M>
+	Matrix& operator+=(const M& x);
+	template<typename M>
+	Matrix& operator-=(const M& x);
+
+	T dot_product(const Matrix_ref<T, 1>&, const Matrix_ref<T, 1>&);
 };
 
-#endif // !1
-
-
+/*template<>
+struct common_type<Quad, long double> {
+	using type = Quad;
+};*/
+#endif // _MATRIX_H_
